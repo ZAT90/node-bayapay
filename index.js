cd3f0e054cb19b051e740378a8c7671331fe4fcc
Zebel Al Tareq <bdzebel@gmail.com>
2024-07-11 14:25:26 +0800

    sample api to load scooters data hardcoded

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+// sample scooter data
+// In a real application, this would come from a database
+const scooters = [
+    {
+      id: 123456,
+      type: 'Scooter',
+      isLocked: true,
+      currentSpeed: 5,
+      battery: 100,
+      status: 'Parking',
+      location: '3.14,0.12',
+      lastUpdated: '2019-07-02 9:00AM'
+    },
+    {
+      id: 234567,
+      type: 'Scooter',
+      isLocked: false,
+      currentSpeed: 5,
+      battery: 50,
+      status: 'Parking',
+      location: '3.14,0.12',
+      lastUpdated: '2019-07-02 9:00AM'
+    },
+    {
+      id: 345678,
+      type: 'Scooter',
+      isLocked: true,
+      currentSpeed: 5,
+      battery: 100,
+      status: 'Parking',
+      location: '3.14,0.12',
+      lastUpdated: '2019-07-02 9:00AM'
+    },
+    {
+      id: 466789,
+      type: 'Scooter',
+      isLocked: true,
+      currentSpeed: 5,
+      battery: 75,
+      status: 'Parking',
+      location: '3.14,0.12',
+      lastUpdated: '2019-07-02 9:00AM'
+    },
+    {
+      id: 789432,
+      type: 'Scooter',
+      isLocked: true,
+      currentSpeed: 5,
+      battery: 0,
+      status: 'Parking',
+      location: '3.14,0.12',
+      lastUpdated: '2019-07-02 9:00AM'
+    }
+  ];
+  
+// Define the endpoint to get all the scooters
+// The request object contains information about the http request to trigger this scooters endpoint
+// the response object is used to send a response back upon http request from client side
+app.get("/api/scooters", (req, res, next) => {
+    res.json(scooters);
+});
+
+// Starting the server
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
